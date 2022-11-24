815bca35a77d3080c1523d712c76494704628bd4
Dilsin Amirtharajah <KillerzDaggers@Outlook.com>
2022-11-24 11:57:20 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttons = document.querySelectorAll("button");
+const body = document.querySelector("body");
+
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function(event) {
+        console.log(event.target.value);
+    })
+}
